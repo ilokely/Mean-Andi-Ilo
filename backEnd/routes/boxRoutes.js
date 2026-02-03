da7d370184ef347ef847f17d59974ef6321a7bce
ilokely <ilokelyy@gmail.com>
2026-02-03 10:34:24 +0100

    Update boxRoutes.js

--- backEnd/routes/boxRoutes.js
+++ backEnd/routes/boxRoutes.js
@@ -5,7 +5,7 @@ const Box = require('../models/Box');
 router.get('/', async (req, res) => {
     try {
         const allbox = await Box.find();
-        res.json(allbox);
+        res.json({ success: true, count: allbox.length, data: allbox});
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -15,10 +15,30 @@ router.post('/', async (req, res) => {
     try {
         const newbox = new Box(req.body);
         await newbox.save();
-        res.status(201).json(newbox);
+        res.status(201).json({ success: true, data:newbox});
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
+});
+
+router.get('/available', async (req, res) => { 
+    try { 
+        const availableBox = await Box.find({ isAvailable: true });
+        res.status(200).json({ success: true, count: availableBox.length, data: availableBox }); 
+    } catch (error) { 
+        console.error('Erreur lors de la récupération des box disponibles:', error);
+        res.status(500).json({ success: false, message: 'Erreur serveur lors de la récupération des box disponibles' });
+    } 
+});
+
+router.get('/unAvailable', async (req, res) => { 
+    try { 
+        const unAvailableBoxes = await Box.find({ isAvailable: false });
+        res.status(200).json({ success: true, count: unAvailableBoxes.length, data: unAvailableBoxes }); 
+    } catch (error) { 
+        console.error('Erreur lors de la récupération des box indisponibles:', error);
+        res.status(500).json({ success: false, message: 'Erreur serveur lors de la récupération des box indisponibles' });
+    } 
 });
 
 module.exports = router;
