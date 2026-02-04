112dfa9c31522500ea8f4d29adb39ccde0a692e3
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2026-02-04 10:16:21 +0300

    routes

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/utilisateur/auth/login/login/login.component';
 
 export const routes: Routes = [
-     { path: '', redirectTo: 'login/client', pathMatch: 'full' }, 
+     { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+     { path: 'login' , component: LoginComponent }
 ];
