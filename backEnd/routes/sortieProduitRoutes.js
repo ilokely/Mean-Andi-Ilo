01a7c86300b7e20a83c81a24df4ea52e558b34ed
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2026-02-17 22:56:45 +0300

    sortieProduit

--- backEnd/routes/sortieProduitRoutes.js
+++ backEnd/routes/sortieProduitRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SortieProduit = require('../models/SortieProduit');
 
-router.get('/getSortieProduits', async(req,res) => {
+router.get('/getAllSortieProduits', async(req,res) => {
     try{
         const sortieProduits = await SortieProduit.find();
         res.json(sortieProduits);
@@ -11,5 +11,58 @@ router.get('/getSortieProduits', async(req,res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+router.get('/getSortieProduitByBoutique/:boutiqueId', async(req,res) => {
+    try{
+        const boutiqueId = req.params.boutiqueId;
+        const sortieProduitByBoutique = await SortieProduit.find({ 'boutique.id': boutiqueId });
+        res.json(sortieProduitByBoutique);
+    }
+    catch(error){
+        res.status(500).json({ message : error.message });
+    }
+})
+
+router.post('/addSortieProduit', async(req,res) => {
+    try{
+        const {
+            produitId,
+            boutiqueId,
+            quantite,
+            prixVente
+        } = req.body;
+
+        const boutique = await Boutique.findById(boutiqueId);
+        const produit = await Produit.findById(produitId);
+
+        if(!boutique || !produit){
+            return res.status(404).json({ error: 'Ressource non trouvée' });
+        }
+
+        const nouvelleSortie = await SortieProduit.create({
+            produit: {
+                id: produit._id,
+                nom: produit.nom,
+                devise: produit.devise
+            },
+            boutique: {
+                id: boutique._id,
+                nomUtilisateur: boutique.nomUtilisateur
+            },
+            quantite,
+            prixVente,
+            date: new Date()
+        });
+
+        res.status(201).json({
+            message: 'Sortie de produit créée avec succès',
+            sortie: nouvelleSortie
+        });
+
+    }
+    catch(error){
+        res.status(500).json({ message: error.message });
+    }
+})
 
 module.exports = router;
