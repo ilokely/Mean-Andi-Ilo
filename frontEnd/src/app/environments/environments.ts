017afdd84f324f3d7f684cec83a33803e48a26d2
ilokely <ilokelyy@gmail.com>
2026-02-25 22:20:57 +0100

    test

--- frontEnd/src/app/environments/environments.ts
+++ frontEnd/src/app/environments/environments.ts
@@ -2,3 +2,8 @@ export const environment = {
     production: true,
     apiUrl: 'https://mean-andi-ilo.onrender.com/'
 };
+
+/* export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:5000'
+}; */
