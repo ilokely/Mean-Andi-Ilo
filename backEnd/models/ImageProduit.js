a5c45367b8d1b818cfbaeb0526446c4454f73ff3
ilokely <ilokelyy@gmail.com>
2026-03-03 12:19:21 +0100

    update

--- backEnd/models/ImageProduit.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const ImageProduitSchema = new mongoose.Schema({
-    path: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('ImageProduit', ImageProduitSchema, 'imageProduit');
